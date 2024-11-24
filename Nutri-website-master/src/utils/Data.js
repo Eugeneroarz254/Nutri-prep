718190af450eec0243ad07e2ee2e9b67e5de357e
Eugeneroarz254 <123081413+Eugeneroarz254@users.noreply.github.com>
2024-11-24 23:03:18 +0300

    Add files via upload

---
+++ Nutri-website-master/src/utils/Data.js
@@ -0,0 +1,32 @@
+import { BsCartCheck } from "react-icons/bs";
+
+export const services = [
+
+    {
+      title: "Affordable Prices",
+      tagline: "Get Factory Default Price",
+      image: "images/service-04.png",
+    },
+
+    {
+      title: "Support 24/7",
+      tagline: "Shop with an expert",
+      image: "images/service-03.png",
+    },
+
+    {
+      title: "Quick Order",
+      tagline: "Easy to place an order",
+      image: <BsCartCheck className="service-icon"/>,
+    },
+    {
+      title: "Affordable Shipping",
+      tagline: "Best shipping prices",
+      image: "images/service.png",
+    },
+    {
+      title: "Daily Surprise Offers",
+      tagline: "Save upto 25% off",
+      image: "images/service-02.png",
+    },
+  ];
